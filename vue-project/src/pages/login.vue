acf84553b7552e40c622da6911b69c5b202da643
Gael <gaelcarrillo995@gmail.com>
2026-04-21 20:25:29 -0700

    made login page

--- vue-project/src/pages/login.vue
+++ vue-project/src/pages/login.vue
@@ -1,5 +1,15 @@
-<template> 
-    <header> </header>
-    <p1> Hello World for CMPS3680</p1>
+<template>
+    <div class="container">
+        <h1>Log In!</h1>
+        <form a="logIn">
+            <label for="email">Email: </label>
+            <input v-model="email"><br>
+            <label for="password">Password: </label>
+            <input v-model="password"><br>
 
+            <button type="submit">Log in!</button>
+
+        </form>
+    </div>
+        
 </template>
