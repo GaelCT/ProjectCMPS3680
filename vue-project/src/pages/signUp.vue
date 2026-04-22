ffee49a9579d29f6c2d1865dbf181f483a2d5062
Gael <gaelcarrillo995@gmail.com>
2026-04-21 20:26:57 -0700

    Added sign up

---
+++ vue-project/src/pages/signUp.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h1>Sign Up!</h1>
+        <form a="signUp">
+            <label for="firstName">First Name: </label>
+            <input v-model="firstName"><br>
+            <label for="lastName">Last Nmae: </label>
+            <input v-model="lastName"><br>
+            <label for="username">Username: </label>
+            <input v-model="username"><br>
+            <label for="email">Email: </label>
+            <input v-model="email"><br>
+            <label for="password">Password: </label>
+            <input v-model="password"><br>
+
+            <button type="submit">Sign Up!</button>
+    </form>
+    </div>
+</template>
+
+
+<!-- Some how add the mysql to be connected to the login.  -->
