1fe62fca5679879e816bac4b0c2e2ad48cc496a8
Gael <gaelcarrillo995@gmail.com>
2026-04-21 20:09:05 -0700

    added some changes and getting thins working

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,20 +1,28 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import login from './pages/login.vue'
 </script>
 
 <template>
-  <header>
+
+<login />
+
+
+
+
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <h1> Hello </h1>
     </div>
   </header>
 
   <main>
     <TheWelcome />
-  </main>
+  </main> -->
 </template>
 
 <style scoped>
---
+++ vue-project/src/pages/login.vue
@@ -0,0 +1,5 @@
+<template> 
+    <header> </header>
+    <p1> Hello World for CMPS3680</p1>
+
+</template>
