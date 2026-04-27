dd39da9e79f2b5a6ecb5d3a3a4e2d0069743ac4c
Jason <jescalante14@csub.edu>
2026-04-27 16:07:45 -0700

    Login & sign up

--- vue-project/src/pages/login.vue
+++ vue-project/src/pages/login.vue
@@ -1,3 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+
+const email = ref('');
+const password = ref('');
+
+const validateForm = () => {
+    if (!/\S+@\S+\.\S+/.test(email.value)) {
+        alert("Invalid email")
+        return false
+    }
+    if (password.value.length < 8) {
+        alert("Invalid password")
+        return false
+    }
+    return true
+}
+
+const logIn = () => {
+    if (!validateForm()) return
+
+    alert("Form submitted (will do more once backend is working)")
+}
+</script>
+
 <template>
     <div class="container">
         <h1>Log In!</h1>
--- vue-project/src/pages/signUp.vue
+++ vue-project/src/pages/signUp.vue
@@ -1,10 +1,40 @@
+<script>
+import { ref } from 'vue';
+
+const firstName = ref('');
+const lastName = ref('');
+const username = ref('');
+const email = ref('');
+const password = ref('');
+
+const validateForm = () => {
+    if(!firstName.value || !lastName.value || !username.value){
+        alert("Name and username is needed")
+        return false
+    }
+    if(!/\S+@\S+\.\S+/.test(email.value)){
+        alert("Invalid email")
+        return false
+    }
+    if(password.value.length < 8){
+        alert("Invalid password, Your password must be at least 8 characters long")
+        return false
+    }
+    return true
+}
+
+const signUp = async() => {
+    if(!validateForm()){
+        return;
+    }
+</script>
 <template>
     <div class="container">
         <h1>Sign Up!</h1>
         <form a="signUp">
             <label for="firstName">First Name: </label>
             <input v-model="firstName"><br>
-            <label for="lastName">Last Nmae: </label>
+            <label for="lastName">Last Name: </label>
             <input v-model="lastName"><br>
             <label for="username">Username: </label>
             <input v-model="username"><br>
@@ -14,6 +44,10 @@
             <input v-model="password"><br>
 
             <button type="submit">Sign Up!</button>
+            <br> 
+            <br>
+            <h2>Don't have an account?</h2>
+            <br>
     </form>
     </div>
 </template>
