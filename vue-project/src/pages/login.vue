942bba7fb4677b7879e2ff1bdab11f1333a708af
Gael <gaelcarrillo995@gmail.com>
2026-04-27 23:06:04 -0700

    why wont it center

--- vue-project/src/pages/login.vue
+++ vue-project/src/pages/login.vue
@@ -25,14 +25,14 @@ const logIn = () => {
 
 <template>
     <div class="container">
-        <h1>Log In!</h1>
+        <h1>Log In</h1>
         <form @submit.prevent="logIn">
             <label for="email">Email: </label>
             <input v-model="email"><br>
             <label for="password">Password: </label>
             <input v-model="password"><br>
 
-            <button type="submit">Log in!</button>
+            <button type="submit">Log in</button>
 
             <h2>Don't have an account?</h2>
                 <button type="button" @click="$router.push('/signUp')" class="SignUp"> Sign Up </button>
--- vue-project/src/pages/main.vue
+++ vue-project/src/pages/main.vue
@@ -6,35 +6,59 @@
 
     </div>
 
-    <div class="container">
-
-    </div>
-
-    <div class="textplate">
+<div class="container">
+   <div class="textplate">
         <span class="icon">+</span>
         <textarea
         v-model="message" 
         name="mainText" 
-        id="" 
         rows="5" 
-        cols="33">
-            Text here...
+        cols="33"
+        placeholder="Text here...">
         </textarea>
     </div>
-        
+</div>
+
 </template>
+
 <style scoped>
+
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    width: 100%;
+    background-color: #1a1a1a;
+}
 
 .textplate{
     display: flex;
-    align-items: flex-start;
+    justify-content: center;
+    align-items: center;
     border: 1px solid #ddd;
-    padding: 8px; 
+    padding: 8px 16px;
+    width: 100%;
+    border-radius: 15px;
+    max-width: 600px;
+    margin: 20px auto;
+    transition: border-color 0.3s;
+}
+
+.textplate:focus-within{
+    border-color: antiquewhite;
+}
+
+textarea{
+    width: 100%;
+    border-radius: 15px;
 }
 
 .icon{
-    margin-right: 10px;
-    margin-top: 4px;
+    font: 900;
+    font-size: xx-large;
+
 }
+
 
 </style>
