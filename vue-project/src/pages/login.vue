9b4280c41b2fba2037b1e0b90365d835cfe6bc12
davalos97 <97danielavalos@gmail.com>
2026-05-01 03:12:15 +0000

    users showing

--- vue-project/src/pages/login.vue
+++ vue-project/src/pages/login.vue
@@ -1,8 +1,4 @@
 <script setup>
-<<<<<<< HEAD
-=======
-// import './login.css'
->>>>>>> 03dcc2fabb6d58cd1b059fb9bd5c5ad674ab13ae
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
@@ -47,10 +43,6 @@ const logIn = async () => {
 
 <template>
     <div class="container">
-<<<<<<< HEAD
-=======
-        <img src="ssh://jescalante/home/jescalante/dro/ProjectCMPS3680/vue-project/src/assets/dro.png" alt="Dro logo" class="logo">
->>>>>>> 03dcc2fabb6d58cd1b059fb9bd5c5ad674ab13ae
         <h1>Log In</h1>
         <form @submit.prevent="logIn">
             <label for="email">Email: </label>
