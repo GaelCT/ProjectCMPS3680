4f073f856b61e214fda1eedf53bde40d74538990
GaelCT <gaelcarrillo995@gmail.com>
2026-05-02 19:54:31 +0000

    random errors

--- vue-project/src/pages/main.vue
+++ vue-project/src/pages/main.vue
@@ -8,26 +8,18 @@ if shift and enter then not enter.
 -->
 
 <script setup>
-<<<<<<< HEAD
+
 //both versions were being used . . .
 import { useEventSource } from './node_modules/@vueuse/core'; //used to catch the sse stream with JSON
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import stream from '../stream.php';
-=======
-import { useEventSource } from '@vueuse/core';
-import { ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
->>>>>>> cd49daa8593ee8550e81ea806f34c7623653cfc0
 
 const router = useRouter();
 const username = ref('');
 const message = ref('');
 const users = ref([]);
-<<<<<<< HEAD
 const chatHistory = ref([]);
-=======
->>>>>>> cd49daa8593ee8550e81ea806f34c7623653cfc0
 
 const { status, data, error, close } = useEventSource('/api/sse-endpoint');
 
@@ -54,8 +46,6 @@ const logOut = () => {
     router.push('/')
 }
 
-<<<<<<< HEAD
-
 
 // Event Source
 const EndpointA = new EventSource('https://jescalante.cs3680.com/stream.php', {
@@ -71,8 +61,6 @@ EndpointA.onmessage = function(event) {
 
 
 
-=======
->>>>>>> cd49daa8593ee8550e81ea806f34c7623653cfc0
 </script>
 <template>
 
