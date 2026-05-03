efb46687da42181894eb22494b759571e722d177
GaelCT <gaelcarrillo995@gmail.com>
2026-05-03 00:38:40 +0000

    more syntax

--- vue-project/src/pages/main.vue
+++ vue-project/src/pages/main.vue
@@ -10,7 +10,7 @@ if shift and enter then not enter.
 <script setup>
 
 //both versions were being used . . .
- import { useEventSource } from './node_modules/@vueuse/core'; //used to catch the sse stream with JSON
+ //import { useEventSource } from './node_modules/@vueuse/core'; //used to catch the sse stream with JSON
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { vBtn } from 'vuetify/components';
--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/dro/',
+    base: '/',
     server: {
         allowedHosts: ['jescalante.cs3680.com'],
     },
